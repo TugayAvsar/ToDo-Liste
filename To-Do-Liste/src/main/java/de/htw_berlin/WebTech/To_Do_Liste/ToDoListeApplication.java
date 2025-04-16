052b7d83e2c7cbe0e1f983dc3585f232ac7d0f0f
TAvsar <tugay.avsar@gmx.de>
2025-04-16 10:56:11 +0200

    Einrichtung

---
+++ To-Do-Liste/src/main/java/de/htw_berlin/WebTech/To_Do_Liste/ToDoListeApplication.java
@@ -0,0 +1,13 @@
+package de.htw_berlin.WebTech.To_Do_Liste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoListeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoListeApplication.class, args);
+	}
+
+}
---
+++ To-Do-Liste/src/test/java/de/htw_berlin/WebTech/To_Do_Liste/ToDoListeApplicationTests.java
@@ -0,0 +1,13 @@
+package de.htw_berlin.WebTech.To_Do_Liste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoListeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
